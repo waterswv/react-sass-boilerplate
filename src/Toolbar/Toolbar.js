5cd1a0ea6190f6b53764cf1aeff2f1c515c25533
Waterswv <bmierke@gmail.com>
2018-08-14 22:51:52 -0700

    added active class for selected navbar item

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -7,10 +7,12 @@ const Toolbar = props => (
       <div className="toolbar__toggle-button"><SideDrawerToggleButton click={props.drawerToggleClickHandler}/></div>
       <div className="toolbar__logo"><a href="/">The Logo</a></div>
       <div className="spacer" />
-        <div className="toolbar__navigation-items">
+        <div className="toolbar__navigation-items" >
           <ul>
-            <li><a href="/">Products</a></li>
-            <li><a href="/">Blog</a></li>
+            <li className="active"><a href="/"><div className="item-text">Things about Wine</div></a></li>
+            <li><a href="/"><div className="item-text">Level Up</div></a></li>
+            <li><a href="/"><div className="item-text">Blog</div></a></li>
+            <li><a href="/"><div className="item-text">P</div></a></li>
           </ul>
         </div>
     </nav>
